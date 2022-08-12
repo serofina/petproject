17ea8a2f8dddc75b86460fb8394478f6519eb04c
Mark Mullett <mark.f.mullett@gmail.com>
2022-08-12 11:11:09 -0700

    updated all links and routes
    lots of small fixes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,50 +5,65 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-router.get("/about.html", (req, res) => {
+router.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/about.html"));
 });
 
-router.get("/booking.html", (req, res) => {
+router.get("/booking", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/booking.html"));
 });
-router.get("/contact.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/contact.html"));
-});
-router.get("/customerform.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/customerform.html"));
-});
-router.get("/faq.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/faq.html"));
-});
-router.get("/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-router.get("/login.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/login.html"));
-});
-router.get("/petform.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/petform.html"));
-});
-router.get("/policy.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/policy.html"));
-});
-router.get("/registration.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/registration.html"));
-});
-router.get("/requirements.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/requirements.html"));
-});
-router.get("/sendemail.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/sendemail.html"));
-});
-router.get("/sendnewsletter.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/sendnewsletter.html"));
-});
-router.get("/service.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/service.html"));
+
+router.get("/blog", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/blog.html"));
 });
 
+router.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/contact.html"));
+});
+
+router.get("/customerform", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/customerform.html"));
+});
+
+router.get("/faqs", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/faqs.html"));
+});
+
+router.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/login.html"));
+});
+
+router.get("/petform", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/petform.html"));
+});
+
+router.get("/policy", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/policy.html"));
+});
+
+router.get("/registration", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/registration.html"));
+});
+
+router.get("/requirements", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/requirements.html"));
+});
+
+router.get("/sendemail", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/sendemail.html"));
+});
+
+router.get("/sendnewsletter", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/sendnewsletter.html"));
+});
+
+router.get("/service", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/service.html"));
+});
 
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/404.html"));
