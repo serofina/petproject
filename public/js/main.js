98c8edbb8c8698fad66a0c791ebc164898a29604
mfmullett <74468038+marknzayra@users.noreply.github.com>
2022-08-23 06:51:45 -0700

    Revert "Serofinatable"

--- public/js/main.js
+++ public/js/main.js
@@ -111,32 +111,34 @@ if (timeNow >= openTimex && timeNow <= closeTimex) {
 }
 // End Opening Hours
 
+
 // begin send email function
 
-const sendMail = (email, subject, message) => {
+const sendMail = ((email, subject, message) => {
   let myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
   console.log(email, subject);
 
   let raw = JSON.stringify({
-    email: email,
-    subject: subject,
-    message: message,
+    "email": email,
+    "subject": subject,
+    "message": message
   });
 
   let requestOptions = {
-    method: "POST",
+    method: 'POST',
     headers: myHeaders,
     body: raw,
-    redirect: "follow",
+    redirect: 'follow'
   };
 
   fetch("/api/mailto", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-};
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+});
 
 //end send email  function
 
@@ -147,25 +149,32 @@ if (form_email) {
   form_email.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    console.log("event", event);
+    console.log('event', event);
+
 
     const email = document.querySelector("#email-address").value;
     const subject = document.querySelector("#email-subject").value;
     const message = document.querySelector("#email-message").value;
 
+
     sendMail(email, subject, message);
-  });
+
+  })
 }
+
 
 //end send email
 
+
 //begin add user to mailing list
+
 
 const form_newsletter = document.querySelector("#newsletter-add");
 
 if (form_newsletter) {
   form_newsletter.addEventListener("submit", (event) => {
     event.preventDefault();
+
 
     const name = document.querySelector("#newsletter-name").value;
     const email = document.querySelector("#newsletter-email").value;
@@ -175,31 +184,32 @@ if (form_newsletter) {
     myHeaders.append("Content-Type", "application/json");
 
     let raw = JSON.stringify({
-      name: name,
-      email: email,
+      "name": name,
+      "email": email
     });
 
     let requestOptions = {
-      method: "POST",
+      method: 'POST',
       headers: myHeaders,
       body: raw,
-      redirect: "follow",
+      redirect: 'follow'
     };
 
     fetch("/api/newsletterAddMember", requestOptions)
-      .then((response) => response.text())
-      .then((result) => {
+      .then(response => response.text())
+      .then(result => {
         if (result) {
-          alert("Thank You for registering for our news letter.");
+          alert("Thank You for registering for our news letter.")
         } else {
-          alert(
-            "Sorry there was a problem, Please try again of contact our staff at email@pethotel.com."
-          );
-        }
+          alert("Sorry there was a problem, Please try again of contact our staff at email@pethotel.com.")
+        };
       })
-      .catch((error) => console.log("error", error));
+      .catch(error => console.log('error', error));
+
+
 
     form_newsletter.reset();
+
   });
 }
 
@@ -207,35 +217,42 @@ if (form_newsletter) {
 
 //send out news-letter emails
 
+
 const newsletter_email = document.querySelector("#send-newsletter");
 
 if (newsletter_email) {
   newsletter_email.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    console.log("event", event);
+    console.log('event', event);
 
+
+    
     const subject = document.querySelector("#newsletter-subject").value;
     const message = document.querySelector("#newsletter-message").value;
 
     let requestOptions = {
-      method: "GET",
-      redirect: "follow",
+      method: 'GET',
+      redirect: 'follow'
     };
-
+    
     fetch("/api/newsletterSubmit", requestOptions)
-      .then((response) => response.json())
-      .then((body) => {
-        body.result.forEach((element) => {
-          const script = `Hello ${element.name}, \n\n Thanks you for subscribing to our newsletter \n\n ${message}`;
+      .then(response => response.json())
+      .then(body => {
+        body.result.forEach(element => {
+          const script = `Hello ${element.name}, \n\n Thanks you for subscribing to our newsletter \n\n ${message}`
           sendMail(element.email, subject, script);
         });
+      
       })
-      .catch((error) => console.log("error", error));
+      .catch(error => console.log('error', error));
 
-    newsletter_email.reset();
-  });
+      newsletter_email.reset();      
+  })
 }
+
+
+
 
 //end send news-letter emails
 
@@ -246,50 +263,48 @@ const bookingrequest = document.querySelector("#booking-form");
 if (bookingrequest) {
   bookingrequest.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    
     const name = document.querySelector("#booking-name").value;
     const email = document.querySelector("#booking-email").value;
     const date = document.querySelector("#booking-date").value;
     const time = document.querySelector("#booking-time").value;
     const service = document.querySelector("#booking-service").value;
-
+    
     const bookingemail = "booking@pethotel.com";
-    const subject = `BOOKING REQUEST ${name} ${email} ${date} ${time} ${service};`;
+    const subject = `BOOKING REQUEST ${name} ${email} ${date} ${time} ${service};`
     const message = `${name} would like to book ${service} on ${date} at ${time}. \n\n His contact is ${email}`;
-
+   
     sendMail(bookingemail, subject, message);
-    alert(
-      "Your request has been set to our staff. If you have any questions, please contact us at 619-555-1234 or info@pet-hotel.com."
-    );
-    bookingrequest.reset();
-  });
+    alert("Your request has been set to our staff. If you have any questions, please contact us at 619-555-1234 or info@pet-hotel.com.")
+    bookingrequest.reset();      
+  })
 }
 
 // end send a booking request
 
-//start contact us
+//start contact us 
+
 
 const contactus = document.querySelector("#contact-form");
 
 if (contactus) {
   contactus.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    
     const name = document.querySelector("#contact-name").value;
     const email = document.querySelector("#contact-email").value;
     const subjectcontact = document.querySelector("#contact-subject").value;
     const messagecontact = document.querySelector("#contact-message").value;
 
+    
     const contactemail = "contact@pethotel.com";
-    const subject = `CONTACT REQUEST ${name}, ${email}, ${subjectcontact};`;
+    const subject = `CONTACT REQUEST ${name}, ${email}, ${subjectcontact};`
     const message = `${name} would like more information on ${subjectcontact}\n\n ${messagecontact}. \n\n His contact is: \n${email}`;
-
+   
     sendMail(contactemail, subject, message);
-    alert(
-      "Your request has been set to our staff. If you have any questions, please contact us at 619-555-1234."
-    );
-    contactus.reset();
-  });
+    alert("Your request has been set to our staff. If you have any questions, please contact us at 619-555-1234.")
+    contactus.reset();      
+  })
 }
 //end contact us
 
@@ -299,71 +314,76 @@ const registration = document.querySelector("#registration-form");
 if (registration) {
   registration.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    
     const name = document.querySelector("#registration-fullname").value;
     const email = document.querySelector("#registration-email").value;
     const password = document.querySelector("#registration-pass").value;
     const RPassword = document.querySelector("#registration-repeat").value;
 
     let myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Content-Type", "application/json");
 
-    if (password === RPassword && password.length > 5) {
-      let raw = JSON.stringify({
-        name: name,
-        email: email,
-        password: password,
-      });
+      if(password===RPassword && password.length>5){
+          let raw = JSON.stringify({
+            "name": name,
+            "email": email,
+            "password": password
+          });
 
-      let requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow",
-      };
+          let requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+          };
 
-      fetch("/api/register", requestOptions)
-        .then((response) => response.json())
-        .then((result) => {
-          console.log("********result", result);
-          if (result.success) {
-            window.location.replace("/login");
-          } else {
-            alert("Unable to create an account. Please try again.");
-          }
-        })
-        .catch((error) => console.error("error", error));
-      registration.reset();
-    } else {
-      alert("Passwords do not match or needs to be longer than 5");
-    }
-  });
+          fetch("/api/register", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+              console.log("********result",result);
+              if (result.success) {
+                window.location.replace("/login");
+              } else {
+                alert('Unable to create an account. Please try again.');
+              }
+            })
+            .catch(error => console.error('error', error));
+            registration.reset(); 
+      }else{
+          alert("Passwords do not match or needs to be longer than 5") 
+      }
+  })
 }
 //END REGISTER
 
 //go to customer form after sign in
 
-const customerHomePage = () => {
-  const token = localStorage.getItem("accessToken");
+const customerHomePage = ()=> {
+
+
+  const token = (localStorage.getItem('accessToken'));
   // console.log(token);
+
 
   let myHeaders = new Headers();
   myHeaders.append("Authorization", `Bearer ${token}`);
   let requestOptions = {
-    method: "GET",
+    method: 'GET',
     headers: myHeaders,
-    redirect: "follow",
+    redirect: 'follow'
   };
-
+  
   fetch("/auth", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      if (result.authenticated) {
-        window.location.pathname = "/customerform";
+    .then(response => response.json())
+    .then(result => {
+      if (result.authenticated){
+        window.location.pathname = ("/customerform");
       }
     })
-    .catch((error) => console.log("error", error));
-};
+    .catch(error => console.log('error', error));
+ 
+ 
+}
 //end go to customer form
 
 //sign in
@@ -372,40 +392,41 @@ const sigin = document.querySelector("#login-form");
 if (sigin) {
   sigin.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    
     const email = document.querySelector("#signin-email").value;
     const password = document.querySelector("#signin-password").value;
 
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-
+    
     let raw = JSON.stringify({
-      email: email,
-      password: password,
+      "email": email,
+      "password": password
     });
-
+    
     let requestOptions = {
-      method: "POST",
+      method: 'POST',
       headers: myHeaders,
       body: raw,
-      redirect: "follow",
+      redirect: 'follow'
     };
-
+    
     fetch("/api/signin", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
+      .then(response => response.json())
+      .then(result => {
         if (result.success) {
           console.log(result.accessToken);
-          localStorage.setItem("accessToken", result.accessToken);
+          localStorage.setItem('accessToken', result.accessToken);
           customerHomePage();
-        } else {
+         }else{
           window.location = "/login";
-          alert("Login Not found!");
+          alert("Login Not found!")
         }
       })
-      .catch((error) => console.log("error", error));
-    // sigin.reset();
-  });
+      .catch(error => console.log('error', error));
+      // sigin.reset(); 
+    
+  })
 }
 
 //end sign in
@@ -416,104 +437,104 @@ const customerform = document.querySelector("#customerForm");
 
 if (customerform) {
   //customerform.addEventListener('DOMContentLoaded', (event) => {
-  // event.preventDefault();
+    // event.preventDefault();
 
-  const token = localStorage.getItem("accessToken");
-
+    
+  const token = (localStorage.getItem('accessToken'));
+  
   let myHeaders = new Headers();
   myHeaders.append("Authorization", `Bearer ${token}`);
 
   let requestOptions = {
-    method: "GET",
+    method: 'GET',
     headers: myHeaders,
-    redirect: "follow",
+    redirect: 'follow'
   };
 
   fetch("api/customerinformation", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
+    .then(response => response.json())
+    .then(result => {
       console.log(result);
       document.querySelector("#inputFirstName").value = result[0].firstName;
-      document.querySelector("#inputLastName").value = result[0].lastName;
+      document.querySelector("#inputLastName").value = result[0].lastName; 
       document.querySelector("#inputTelephoneNumber").value = result[0].phone;
-      document.querySelector("#inputEmail").value = result[0].email;
-      document.querySelector("#inputAddress").value = result[0].address;
+      document.querySelector("#inputEmail").value = result[0].email; 
+      document.querySelector("#inputAddress").value= result[0].address;
       document.querySelector("#inputAddress2").value = result[0].address2;
       document.querySelector("#inputCity").value = result[0].city;
       document.querySelector("#inputState").value = result[0].state;
-      document.querySelector("#inputZip").value = result[0].zip;
-      document.querySelector("#inputEmergencyContact").value =
-        result[0].emergencyContact;
-      document.querySelector("#inputEmergencyTelephoneNumber").value =
-        result[0].emergencyPhone;
+      document.querySelector("#inputZip").value= result[0].zip;
+      document.querySelector("#inputEmergencyContact").value = result[0].emergencyContact;
+      document.querySelector("#inputEmergencyTelephoneNumber").value = result[0].emergencyPhone;  
     })
-    .catch((error) => console.log("error", error));
-  // customerform.reset();
-
-  // })
+    .catch(error => console.log('error', error));
+        // customerform.reset(); 
+      
+    // })
 }
 
+
 //end update customer information
+
 
 //get customer information DOES not trigger event
 
 if (customerform) {
-  customerform.addEventListener("submit", (event) => {
+  customerform.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("loaded");
+    console.log('loaded');
 
     //create all the varaibles incase they don't come back from the server
+  
+  const firstName = document.querySelector("#inputFirstName").value;
+  const lastName = document.querySelector("#inputLastName").value;
+  const phone = document.querySelector("#inputTelephoneNumber").value;
+  const email= document.querySelector("#inputEmail").value;
+  const address = document.querySelector("#inputAddress").value;
+  const address2 = document.querySelector("#inputAddress2").value;
+  const city = document.querySelector("#inputCity").value;
+  const state = document.querySelector("#inputState").value;
+  const zip = document.querySelector("#inputZip").value;
+  const emergencyContact = document.querySelector("#inputEmergencyContact").value;
+  const emergencyPhone = document.querySelector("#inputEmergencyTelephoneNumber").value;
+    
+  const token = (localStorage.getItem('accessToken'));
+  
+  let myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${token}`);
+  myHeaders.append("Content-Type", "application/json");
 
-    const firstName = document.querySelector("#inputFirstName").value;
-    const lastName = document.querySelector("#inputLastName").value;
-    const phone = document.querySelector("#inputTelephoneNumber").value;
-    const email = document.querySelector("#inputEmail").value;
-    const address = document.querySelector("#inputAddress").value;
-    const address2 = document.querySelector("#inputAddress2").value;
-    const city = document.querySelector("#inputCity").value;
-    const state = document.querySelector("#inputState").value;
-    const zip = document.querySelector("#inputZip").value;
-    const emergencyContact = document.querySelector(
-      "#inputEmergencyContact"
-    ).value;
-    const emergencyPhone = document.querySelector(
-      "#inputEmergencyTelephoneNumber"
-    ).value;
 
-    const token = localStorage.getItem("accessToken");
+      let raw = JSON.stringify({
+        "firstName": firstName,
+        "lastName": lastName,
+        "phone": phone,
+        "address": address,
+        "address2": address2,
+        "city": city,
+        "state": state,
+        "zip": zip,
+        "emergencyContact": emergencyContact,
+        "emergencyPhone": emergencyPhone
+      });
 
-    let myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${token}`);
-    myHeaders.append("Content-Type", "application/json");
+      let requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
 
-    let raw = JSON.stringify({
-      firstName: firstName,
-      lastName: lastName,
-      phone: phone,
-      address: address,
-      address2: address2,
-      city: city,
-      state: state,
-      zip: zip,
-      emergencyContact: emergencyContact,
-      emergencyPhone: emergencyPhone,
-    });
-
-    let requestOptions = {
-      method: "PUT",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
-
-    fetch("/api/customerform", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.success) alert("Thank you for updating your information");
-      })
-      .catch((error) => console.log("error", error));
-    customerform.reload();
-  });
+      fetch("/api/customerform", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          if (result.success) alert("Thank you for updating your information")
+        })
+        .catch(error => console.log('error', error));
+      customerform.reload(); 
+      
+    })
 }
+
 
 //end update customer information
--- server.js
+++ server.js
@@ -1,20 +1,257 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
+const mysql = require("mysql2");
+const nodemailer = require("nodemailer");
 const htmlRoute = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes.js");
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const { request } = require('http');
+const saltRounds = 10;
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+
+
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
 
 const app = express();
+app.use(express.urlencoded({ extended: false })); // server to accept strings and array from url
+app.use(express.json()); // allows for the server to accept json objs
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
-app.use(apiRoutes);
+
+app.post("/api/signin", async function (req, res) {
+	const { email, password} = req.body
+	connection.query(`SELECT * FROM users, login where users.iduser=login.iduser and email = ?`, [email],
+	async function(error, results, fields) {
+					if (error) {
+						res.status(401).json({success: false, message: "Login not successful" })
+					}else{
+						if(results.length > 0){
+							const isvalid = bcrypt.compareSync(password,results[0].hash);
+										if (isvalid){
+											const username = results[0].username;
+											const email = results[0].email;
+											const iduser = results[0].iduser; 
+											const user ={name: username, email: email, iduser: iduser}
+											const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)
+											res.json({ success: true, accessToken: accessToken});
+										}else{
+											res.status(500).json({ success: false, Error: 'Email and Pass do not match.'});
+										}      
+						}else{
+							res.status(206).json({ success: false, message: "User not found" })
+						}
+				}
+			});
+	})
+	
+	
+	
+	app.post("/api/register", (req, res) => {
+
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password, saltRounds);
+      connection.query(
+      `call addUser ("${name}", "${email}", "${hash}")`,
+        function (err, result) {
+          console.log(result);
+          if(result[0][0].hasOwnProperty("fail")) {
+						res.status(500).json({ success: false, error: err });
+          }else{
+            res.json({ success: true });
+          }
+      });
+   })
+      
+
+   app.put("/api/customerform" ,authenticateToken, (req, res) => {
+		let { iduser } = req.user
+    let { firstName, lastName, phone, address, address2, city, state, zip, emergencyContact, emergencyPhone} = req.body;
+
+    connection.query(
+    `UPDATE customerinfo SET firstName = "${firstName}", lastName ="${lastName}", 
+    phone = "${phone}", address = "${address}", address2 = "${address2}", city = "${city}", 
+    state = "${state}", zip = "${zip}", emergencyContact = "${emergencyContact}", emergencyphone = "${emergencyPhone}"
+    WHERE (iduser = '${iduser}');`,
+     function (err, result) {
+        if(result.affectedRows === 0|| err) {
+          res.status(500).json({ success: false, error: err });
+        }else{
+          res.json({ success: true });
+        }
+      });
+  })
+
+
+app.post('/api/mailto', (req, res) => {
+	let { email, subject, message } = req.body;
+	console.log(email, subject)
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.ethereal.email',
+		port: 587,
+		auth: {
+			user: 'boris66@ethereal.email',
+			pass: 'b6wVK8CRsJ5XDMJsTa'
+		}
+	});
+
+	let mailOptions = {
+		from: 'pethotelnational@Ethereal.com ',
+		to: email,
+		subject: subject,
+		text: message
+	};
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			res.json(error);
+		} else {
+			res.json('Email sent: ' + info.response);
+		}
+	});
+})
+
+
+
+app.post("/api/newsletterAddMember", (req, res) => {
+	let { name, email } = req.body;
+	connection.query(
+		"INSERT INTO newsletter (name, email) VALUES ( ? , ? )",
+		[name, email],
+		(err) => {
+			if (err) {
+				res.status(404).json({ error: err });
+			}
+			res.json({
+				success: true,
+			});
+		}
+	);
+});
+
+app.get("/api/newsletterSubmit", (req, res) => {
+	connection.query("SELECT * FROM newsletter", function (err, result) {
+			if (err) {
+				res.status(404).json({ error: err });
+			}
+			res.json({ result });
+		}
+	);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// app.get('/sendnewsletter.html', (req, res) => {
+// 	res.sendFile(path.join(__dirname, "/public/sendnewsletter.html"));
+// });
+
+app.post("/api/signin", (req, res) => {
+  res.json("signin");
+});
+
+app.post("/api/mailto", (req, res) => {
+  let { email, subject, message } = req.body;
+  console.log(email, subject);
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "boris66@ethereal.email",
+      pass: "b6wVK8CRsJ5XDMJsTa",
+    },
+  });
+
+  let mailOptions = {
+    from: "pethotelnational@Ethereal.com ",
+    to: email,
+    subject: subject,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json("Email sent: " + info.response);
+    }
+  });
+});
+
+app.post("/api/newsletterAddMember", (req, res) => {
+	let { name, email } = req.body;
+	connection.query(
+		"INSERT INTO newsletter (name, email) VALUES ( ? , ? )",
+		[name, email],
+		(err) => {
+			if (err) {
+				res.status(404).json({ error: err });
+			}
+			res.json({
+				success: true,
+			});
+		}
+	);
+});
+
+app.get("/api/newsletterSubmit", (req, res) => {
+	connection.query("SELECT * FROM newsletter", function (err, result) {
+			if (err) {
+				res.status(404).json({ error: err });
+			}
+			res.json({ result });
+		}
+	);
+});
+
+app.get("/api/customerinformation",authenticateToken, (req, res) => {
+	let { iduser } = req.user
+	connection.query("SELECT * FROM users, customerinfo where users.iduser= customerinfo.iduser and users.iduser = ?;", [iduser], 
+	(err, result) => {
+		if (err) res.status(404).json({ error: 'No user' });
+		res.json(result);
+	});
+});
+
+
+app.get("/auth",authenticateToken, (req, res) => {
+	res.json({authenticated: true});
+});
+
+app.get("/petform",authenticateToken,(req, res) => {
+  res.sendFile(path.join(__dirname, "./public/petform.html"));
+});
+
+
+function authenticateToken(req, res, next) {
+	const authHeader = req.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
+
+	if (token===null) return res.sendStatus(401)
+
+	jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+		if (err) return res.sendStatus(403);
+		req.user = user
+		next()
+	})
+}
+
 app.use(htmlRoute);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`app in running on node ${PORT}`);
